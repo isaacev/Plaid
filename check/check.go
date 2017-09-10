36b20ec088c8d08d8bfff16c8802178205dd6c0c
Isaac Evavold <isaacev@users.noreply.github.com>
2017-09-10 16:36:37 -0500

    implement and test Scope methods

--- check/check.go
+++ check/check.go
@@ -1,12 +1,88 @@
 package check
 
 import (
+	"fmt"
 	"plaid/parser"
+	"sort"
+)
+
 )
 
 // Check takes an existing abstract syntax tree and performs type checks and
 // other correctness checks. It returns a list of any errors that were
 // discovered inside the AST
-func Check(prog parser.Program) []error {
-	return nil
+func Check(prog parser.Program) *Scope {
+	global := makeScope(nil)
+	checkProgram(global, prog)
+	return global
+}
+
+// Scope tracks the symbol table and other data used during the check
+type Scope struct {
+	parent    *Scope
+	variables map[string]Type
+	Errs      []error
+}
+
+func (s *Scope) hasParent() bool {
+	return (s.parent != nil)
+}
+
+func (s *Scope) registerVariable(name string, typ Type) {
+	s.variables[name] = typ
+}
+
+func (s *Scope) hasVariable(name string) bool {
+	_, exists := s.variables[name]
+	return exists
+}
+
+func (s *Scope) getVariable(name string) Type {
+	return s.variables[name]
+}
+
+func (s *Scope) addError(err error) {
+	if s.hasParent() {
+		s.parent.addError(err)
+	} else {
+		s.Errs = append(s.Errs, err)
+	}
+}
+
+func (s *Scope) String() string {
+	names := []string{}
+	for name := range s.variables {
+		names = append(names, name)
+	}
+	sort.Strings(names)
+
+	out := "+----------+--------------+\n"
+	out += "| Var      | Type         |\n"
+	out += "| -------- | ------------ |\n"
+	for _, name := range names {
+		out += fmt.Sprintf("| %-8s | %-12s |\n", name, s.variables[name])
+	}
+	out += "+----------+--------------+\n"
+	return out
+}
+
+func makeScope(parent *Scope) *Scope {
+	scope := &Scope{
+		parent,
+		make(map[string]Type),
+		[]error{},
+	}
+
+	return scope
+}
+
+func checkProgram(scope *Scope, prog parser.Program) {
+	for _, stmt := range prog.Stmts {
+		switch stmt := stmt.(type) {
+		default:
+			panic("unknown statement type")
+		}
+	}
+}
+
 }
--- check/check_test.go
+++ check/check_test.go
@@ -1,13 +1,77 @@
 package check
 
 import (
+	"fmt"
 	"plaid/parser"
 	"testing"
 )
 
 func TestCheckMain(t *testing.T) {
-	errs := Check(parser.Program{})
-	expectNoErrors(t, errs)
+	scope := Check(parser.Program{})
+	expectNoErrors(t, scope.Errs)
+}
+
+func TestScopeHasParent(t *testing.T) {
+	root := makeScope(nil)
+	child := makeScope(root)
+	expectBool(t, root.hasParent(), false)
+	expectBool(t, child.hasParent(), true)
+}
+
+func TestScopeRegisterVariable(t *testing.T) {
+	scope := makeScope(nil)
+	scope.registerVariable("foo", TypeIdent{"Bar"})
+	typ, exists := scope.variables["foo"]
+	if exists {
+		expectEquivalentType(t, typ, TypeIdent{"Bar"})
+	} else {
+		t.Errorf("Expected key '%s' in Scope#variables, none found", "foo")
+	}
+}
+
+func TestScopeHasVariable(t *testing.T) {
+	scope := makeScope(nil)
+	scope.registerVariable("foo", TypeIdent{"Bar"})
+	expectBool(t, scope.hasVariable("foo"), true)
+	expectBool(t, scope.hasVariable("baz"), false)
+}
+
+func TestScopeGetVariable(t *testing.T) {
+	scope := makeScope(nil)
+	scope.registerVariable("foo", TypeIdent{"Bar"})
+	expectEquivalentType(t, scope.getVariable("foo"), TypeIdent{"Bar"})
+	expectNil(t, scope.getVariable("baz"))
+}
+
+func TestScopeAddError(t *testing.T) {
+	scope := makeScope(nil)
+	expectNoErrors(t, scope.Errs)
+	scope.addError(fmt.Errorf("a semantic analysis error"))
+	expectAnError(t, scope.Errs[0], "a semantic analysis error")
+
+	root := makeScope(nil)
+	child := makeScope(root)
+	expectNoErrors(t, root.Errs)
+	expectNoErrors(t, child.Errs)
+	child.addError(fmt.Errorf("a semantic analysis error"))
+	expectNoErrors(t, child.Errs)
+	expectAnError(t, root.Errs[0], "a semantic analysis error")
+}
+
+func TestScopeString(t *testing.T) {
+	scope := makeScope(nil)
+	scope.registerVariable("num", TypeIdent{"Int"})
+	scope.registerVariable("test", TypeIdent{"Bool"})
+	scope.registerVariable("coord", TypeTuple{[]Type{TypeIdent{"Int"}, TypeIdent{"Int"}}})
+
+	expectString(t, scope.String(), `+----------+--------------+
+| Var      | Type         |
+| -------- | ------------ |
+| coord    | (Int Int)    |
+| num      | Int          |
+| test     | Bool         |
++----------+--------------+
+`)
 }
 
 func expectNoErrors(t *testing.T, errs []error) {
@@ -17,5 +81,19 @@ func expectNoErrors(t *testing.T, errs []error) {
 		}
 
 		t.Fatalf("Expected no errors, found %d", len(errs))
+	}
+}
+
+func expectAnError(t *testing.T, err error, msg string) {
+	if err == nil {
+		t.Errorf("Expected an error '%s', got no errors", err)
+	} else if msg != err.Error() {
+		t.Errorf("Expected '%s', got '%s'", msg, err)
+	}
+}
+
+func expectNil(t *testing.T, got interface{}) {
+	if got != nil {
+		t.Errorf("Expected nil, got '%v'", got)
 	}
 }
