ddb68e08de1b85e9762536392824c342f962332a
Isaac Evavold <isaacev@users.noreply.github.com>
2017-09-11 02:17:05 -0500

    move Scope implementation and tests into own files

--- check/check_test.go
+++ check/check_test.go
@@ -1,7 +1,6 @@
 package check
 
 import (
-	"fmt"
 	"plaid/lexer"
 	"plaid/parser"
 	"testing"
@@ -12,87 +11,6 @@ var nop = lexer.Token{}
 func TestCheckMain(t *testing.T) {
 	scope := Check(parser.Program{})
 	expectNoErrors(t, scope.Errors())
-}
-
-func TestScopeHasParent(t *testing.T) {
-	root := makeScope(nil, nil)
-	child := makeScope(root, nil)
-	expectBool(t, root.hasParent(), false)
-	expectBool(t, child.hasParent(), true)
-}
-
-func TestScopeRegisterVariable(t *testing.T) {
-	scope := makeScope(nil, nil)
-	scope.registerVariable("foo", TypeIdent{"Bar"})
-	typ, exists := scope.values["foo"]
-	if exists {
-		expectEquivalentType(t, typ, TypeIdent{"Bar"})
-	} else {
-		t.Errorf("Expected key '%s' in Scope#variables, none found", "foo")
-	}
-}
-
-func TestScopeHasVariable(t *testing.T) {
-	scope := makeScope(nil, nil)
-	scope.registerVariable("foo", TypeIdent{"Bar"})
-	expectBool(t, scope.hasVariable("foo"), true)
-	expectBool(t, scope.hasVariable("baz"), false)
-}
-
-func TestScopeGetVariable(t *testing.T) {
-	scope := makeScope(nil, nil)
-	scope.registerVariable("foo", TypeIdent{"Bar"})
-	expectEquivalentType(t, scope.getVariable("foo"), TypeIdent{"Bar"})
-	expectNil(t, scope.getVariable("baz"))
-}
-
-func TestScopeAddError(t *testing.T) {
-	scope := makeScope(nil, nil)
-	expectNoErrors(t, scope.Errors())
-	scope.addError(fmt.Errorf("a semantic analysis error"))
-	expectAnError(t, scope.errs[0], "a semantic analysis error")
-
-	root := makeScope(nil, nil)
-	child := makeScope(root, nil)
-	expectNoErrors(t, root.Errors())
-	expectNoErrors(t, child.Errors())
-	child.addError(fmt.Errorf("a semantic analysis error"))
-	expectNoErrors(t, child.Errors())
-	expectAnError(t, root.errs[0], "a semantic analysis error")
-}
-
-func TestScopeString(t *testing.T) {
-	scope := makeScope(nil, nil)
-	scope.registerVariable("num", TypeIdent{"Int"})
-	scope.registerVariable("test", TypeIdent{"Bool"})
-	scope.registerVariable("coord", TypeTuple{[]Type{TypeIdent{"Int"}, TypeIdent{"Int"}}})
-
-	expectString(t, scope.String(), "num : Int\ntest : Bool\ncoord : (Int Int)\n")
-}
-
-func TestScopeHasPendingReturnType(t *testing.T) {
-	scope := makeScope(nil, nil)
-	expectBool(t, scope.hasPendingReturnType(), false)
-
-	scope = makeScope(nil, TypeIdent{"Int"})
-	expectBool(t, scope.hasPendingReturnType(), true)
-}
-
-func TestScopeGetPendingReturnType(t *testing.T) {
-	scope := makeScope(nil, nil)
-	expectBool(t, scope.getPendingReturnType() == nil, true)
-
-	scope = makeScope(nil, TypeIdent{"Int"})
-	expectEquivalentType(t, scope.getPendingReturnType(), TypeIdent{"Int"})
-}
-
-func TestScopeSetPendingReturnType(t *testing.T) {
-	scope := makeScope(nil, nil)
-	expectBool(t, scope.hasPendingReturnType(), false)
-
-	scope.setPendingReturnType(TypeIdent{"Int"})
-	expectBool(t, scope.pendingReturn.Equals(TypeIdent{"Int"}), true)
-	expectBool(t, scope.hasPendingReturnType(), true)
 }
 
 func TestCheckProgram(t *testing.T) {
