22be233c63174340ca86f32b9856c5be46f71084
Isaac Evavold <isaacev@users.noreply.github.com>
2017-10-08 16:44:54 -0500

    basic debugging CLI

---
+++ plaid.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"plaid/check"
+	"plaid/codegen"
+	"plaid/parser"
+	"plaid/vm"
+)
+
+func main() {
+	showAST := flag.Bool("ast", false, "output abstract syntax tree")
+	showIR := flag.Bool("ir", false, "output intermediate representation")
+	showBC := flag.Bool("bytecode", false, "output bytecode")
+	flag.Parse()
+
+	for _, filename := range flag.Args() {
+		processFile(filename, *showAST, *showIR, *showBC)
+	}
+}
+
+func processFile(filename string, showAST bool, showIR bool, showBC bool) {
+	buf, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	src := string(buf)
+	ast, err := parser.Parse(src)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	if showAST {
+		fmt.Println(ast.String())
+	}
+
+	scope := check.Check(ast)
+	if len(scope.Errors()) > 0 {
+		for i, err := range scope.Errors() {
+			fmt.Printf("%4d %s\n", i, err)
+		}
+		os.Exit(1)
+	}
+
+	ir := codegen.Transform(ast)
+
+	if showIR {
+		fmt.Println(ir.String())
+	}
+
+	mod := codegen.Generate(ir)
+
+	if showBC {
+		fmt.Println(mod.Main.String())
+	}
+
+	vm.Run(mod.Main)
+}
