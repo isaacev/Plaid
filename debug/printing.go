90b4cbf84854d4c8484bba2920d0c5d43d58aec1
ievavold <ievavold@wisc.edu>
2017-10-11 00:27:05 -0500

    add tree pretty printing function
    will help debug type scopes and associated bytecode chunks

---
+++ debug/printing.go
@@ -0,0 +1,90 @@
+package debug
+
+import (
+	"fmt"
+	"strings"
+)
+
+const (
+	pipeVert  = "│"
+	pipeHoriz = "─"
+	cornerNW  = "╭"
+	cornerNE  = "╮"
+	cornerSW  = "╰"
+	cornerSE  = "╯"
+	teeN      = "┴"
+	teeS      = "┬"
+	teeW      = "┤"
+	teeE      = "├"
+)
+
+// StringTree describes the node of a tree that can have 0 or more children
+// each of whom defines a `String() string` function and may have its own
+// children
+type StringTree interface {
+	fmt.Stringer
+	StringChildren() []StringTree
+}
+
+// PrettyTree takes a StringTree and returns a string of the form:
+// ╭
+// ┤ (leaf 1).String()
+// │ ╭
+// ├─┤ (leaf 1.1).String()
+// │ │ ╭
+// │ ╰─┤ (leaf 1.1.1).String()
+// │   ╰
+// │ ╭
+// ╰─┤ (leaf 1.2).String()
+//   ╰
+func PrettyTree(root StringTree) (out string) {
+	children := root.StringChildren()
+	total := len(children)
+	out += brace(root.String(), total == 0)
+	for i, child := range children {
+		if i < total-1 {
+			out += "\n" + indentAndAttach(PrettyTree(child), teeE, pipeVert)
+		} else {
+			out += "\n" + indentAndAttach(PrettyTree(child), cornerSW, " ")
+		}
+	}
+	return out
+}
+
+func brace(source string, close bool) string {
+	var lines []string
+	lines = append(lines, cornerNW+pipeHoriz)
+	for i, line := range split(source) {
+		if i == 0 {
+			lines = append(lines, teeW+" "+line)
+		} else {
+			lines = append(lines, pipeVert+" "+line)
+		}
+	}
+	if close {
+		lines = append(lines, cornerSW+pipeHoriz)
+	}
+	return join(lines)
+}
+
+func split(source string) []string {
+	return strings.Split(source, "\n")
+}
+
+func join(source []string) string {
+	return strings.Join(source, "\n")
+}
+
+func indentAndAttach(source string, connector string, rest string) string {
+	lines := split(source)
+	for i, line := range lines {
+		if i == 0 {
+			lines[i] = pipeVert + " " + line
+		} else if i == 1 {
+			lines[i] = connector + pipeHoriz + line
+		} else {
+			lines[i] = rest + " " + line
+		}
+	}
+	return join(lines)
+}
---
+++ debug/printing_test.go
@@ -0,0 +1,87 @@
+package debug
+
+import (
+	"testing"
+)
+
+type dummy struct {
+	name  string
+	leafs []dummy
+}
+
+func (d dummy) String() string {
+	return d.name
+}
+
+func (d dummy) StringChildren() (s []StringTree) {
+	for _, l := range d.leafs {
+		s = append(s, l)
+	}
+	return s
+}
+
+func TestPrettyTree(t *testing.T) {
+	test := func(root StringTree, exp string) {
+		got := PrettyTree(root)
+		expectString(t, got, exp)
+	}
+
+	d := dummy{name: "d"}
+	c := dummy{name: "c", leafs: []dummy{d}}
+	b := dummy{name: "b"}
+	a := dummy{name: "a", leafs: []dummy{b, c}}
+
+	test(d, "╭─\n┤ d\n╰─")
+	test(a, "╭─\n┤ a\n│ ╭─\n├─┤ b\n│ ╰─\n│ ╭─\n╰─┤ c\n  │ ╭─\n  ╰─┤ d\n    ╰─")
+}
+
+func TestBrace(t *testing.T) {
+	test := func(source string, close bool, exp string) {
+		got := brace(source, close)
+		expectString(t, got, exp)
+	}
+
+	test("", true, "╭─\n┤ \n╰─")
+	test("", false, "╭─\n┤ ")
+	test("foo\nbar\nbaz", true, "╭─\n┤ foo\n│ bar\n│ baz\n╰─")
+	test("foo\nbar\nbaz", false, "╭─\n┤ foo\n│ bar\n│ baz")
+}
+
+func TestSplit(t *testing.T) {
+	expectStringSlice(t, split(""), []string{""})
+	expectStringSlice(t, split("\n"), []string{"", ""})
+	expectStringSlice(t, split("foo\nbar\nbaz"), []string{"foo", "bar", "baz"})
+}
+
+func TestJoin(t *testing.T) {
+	expectString(t, join(nil), "")
+	expectString(t, join([]string{""}), "")
+	expectString(t, join([]string{"", ""}), "\n")
+	expectString(t, join([]string{"foo", "bar", "baz"}), "foo\nbar\nbaz")
+}
+
+func TestIndentAndAttach(t *testing.T) {
+	test := func(source string, connector string, rest string, exp string) {
+		got := indentAndAttach(source, connector, rest)
+		expectString(t, got, exp)
+	}
+
+	test("foo\nbar\nbaz", "╰", " ", "│ foo\n╰─bar\n  baz")
+	test("foo\nbar\nbaz", "├", "│", "│ foo\n├─bar\n│ baz")
+}
+
+func expectString(t *testing.T, got string, exp string) {
+	if got != exp {
+		t.Errorf("Expected '%s', got '%s'", exp, got)
+	}
+}
+
+func expectStringSlice(t *testing.T, got []string, exp []string) {
+	if len(got) != len(exp) {
+		t.Errorf("Expected %d strings, got %d strings", len(exp), len(got))
+	} else {
+		for i := 0; i < len(exp); i++ {
+			expectString(t, got[i], exp[i])
+		}
+	}
+}
