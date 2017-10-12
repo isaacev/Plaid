20c906fe413e4d9acae4479f012d1c7755d4b41d
isaacev <isaacevavold@gmail.com>
2017-10-11 19:16:53 -0500

    redesign Module struct
    - points to ClosureTemplate instead of Bytecode
    - reveals an Export map for communication between modules

--- vm/module.go
+++ vm/module.go
@@ -1,7 +1,18 @@
 package vm
 
-// Module is a collection of functions
+import "plaid/types"
+
+// Module holds all the data necessary to build and evaluate a code module
+// including all child closures and any dependency information
 type Module struct {
-	Main      *Bytecode
-	Functions []*Bytecode
+	Root    *ClosureTemplate
+	Exports map[string]*Export
+}
+
+// Export describes an object made available to other modules. That object is
+// described by a type for use during the type-checking stage of whatever
+// modules use this export
+type Export struct {
+	Type   types.Type
+	Object Object
 }
