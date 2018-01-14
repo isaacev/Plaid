2626aa98b1de34fa350a42fc5b19e87f5b27ed29
isaacev <isaacevavold@gmail.com>
2018-01-14 00:52:38 -0600

    move conversion function into typechecking lib

--- lang/check.go
+++ lang/check.go
@@ -441,3 +441,33 @@ func addTypeError(s Scope, loc Loc, msg string) {
 func (err TypeCheckError) Error() string {
 	return fmt.Sprintf("%s %s", err.Loc, err.Message)
 }
+
+// convertTypeNote transforms a TypeNote struct (used to represent a syntax
+// type notation) into a Type struct (used internally to represent a type)
+func convertTypeNote(note TypeNote) types.Type {
+	switch note := note.(type) {
+	case TypeNoteAny:
+		return types.Any{}
+	case TypeNoteVoid:
+		return types.Void{}
+	case TypeNoteFunction:
+		return types.Function{
+			Params: convertTypeNote(note.Params).(types.Tuple),
+			Ret:    convertTypeNote(note.Ret),
+		}
+	case TypeNoteTuple:
+		elems := []types.Type{}
+		for _, elem := range note.Elems {
+			elems = append(elems, convertTypeNote(elem))
+		}
+		return types.Tuple{Children: elems}
+	case TypeNoteList:
+		return types.List{Child: convertTypeNote(note.Child)}
+	case TypeNoteOptional:
+		return types.Optional{Child: convertTypeNote(note.Child)}
+	case TypeNoteIdent:
+		return types.Ident{Name: note.Name}
+	default:
+		return nil
+	}
+}
--- lang/check_test.go
+++ lang/check_test.go
@@ -456,6 +456,53 @@ func TestConvertTypeSig(t *testing.T) {
 	expectBool(t, convertTypeNote(note) == nil, true)
 }
 
+func TestConvertTypeNote(t *testing.T) {
+	var note TypeNote
+	var blob TypeNote = TypeNoteIdent{Tok: nop, Name: "Blob"}
+	var blub TypeNote = TypeNoteIdent{Tok: nop, Name: "Blub"}
+	var blah TypeNote = TypeNoteIdent{Tok: nop, Name: "Blah"}
+
+	note = TypeNoteVoid{}
+	expectConversion(t, note, "Void")
+
+	note = TypeNoteAny{}
+	expectConversion(t, note, "Any")
+
+	note = TypeNoteFunction{
+		Params: TypeNoteTuple{Tok: nop, Elems: []TypeNote{}},
+		Ret:    TypeNoteVoid{},
+	}
+	expectConversion(t, note, "() => Void")
+
+	note = TypeNoteFunction{
+		Params: TypeNoteTuple{Tok: nop, Elems: []TypeNote{
+			blob,
+			blub,
+		}},
+		Ret: blah,
+	}
+	expectConversion(t, note, "(Blob Blub) => Blah")
+
+	note = TypeNoteList{Tok: nop, Child: blob}
+	expectConversion(t, note, "[Blob]")
+
+	note = TypeNoteOptional{Tok: nop, Child: blah}
+	expectConversion(t, note, "Blah?")
+
+	note = nil
+	got := convertTypeNote(note)
+	if got != nil {
+		t.Errorf("Expected '%v', got '%v'", nil, got)
+	}
+}
+
+func expectConversion(t *testing.T, note TypeNote, exp string) {
+	got := convertTypeNote(note)
+	if got.String() != exp {
+		t.Errorf("Expected '%s', got '%v'", exp, got)
+	}
+}
+
 func makeTok(line int, col int) token {
 	return token{Loc: Loc{Line: line, Col: col}}
 }
