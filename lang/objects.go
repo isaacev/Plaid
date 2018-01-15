1a0e88d4448f0d8fa560d50bdaf35a03e456c689
isaacev <isaacevavold@gmail.com>
2018-01-14 23:36:04 -0600

    remove object pointer receivers

--- lang/objects.go
+++ lang/objects.go
@@ -12,45 +12,45 @@ type Object interface {
 
 type ObjectNone struct{}
 
-func (o *ObjectNone) String() string { return "<none>" }
-func (o *ObjectNone) isObject()      {}
+func (o ObjectNone) String() string { return "<none>" }
+func (o ObjectNone) isObject()      {}
 
 type ObjectInt struct {
 	Val int64
 }
 
-func (o *ObjectInt) String() string { return fmt.Sprintf("%d", o.Val) }
-func (o *ObjectInt) isObject()      {}
+func (o ObjectInt) String() string { return fmt.Sprintf("%d", o.Val) }
+func (o ObjectInt) isObject()      {}
 
 type ObjectStr struct {
 	Val string
 }
 
-func (o *ObjectStr) String() string { return o.Val }
-func (o *ObjectStr) isObject()      {}
+func (o ObjectStr) String() string { return o.Val }
+func (o ObjectStr) isObject()      {}
 
 type ObjectBool struct {
 	Val bool
 }
 
-func (o *ObjectBool) String() string { return fmt.Sprintf("%t", o.Val) }
-func (o *ObjectBool) isObject()      {}
+func (o ObjectBool) String() string { return fmt.Sprintf("%t", o.Val) }
+func (o ObjectBool) isObject()      {}
 
 type ObjectBuiltin struct {
 	Type types.Type
 	Val  func(args []Object) (Object, error)
 }
 
-func (o *ObjectBuiltin) String() string { return "<builtin>" }
-func (o *ObjectBuiltin) isObject()      {}
+func (o ObjectBuiltin) String() string { return "<builtin>" }
+func (o ObjectBuiltin) isObject()      {}
 
 type ObjectFunction struct {
 	// Params   []*UniqueSymbol
 	// Bytecode *Bytecode
 }
 
-func (o *ObjectFunction) String() string { return "<function>" }
-func (o *ObjectFunction) isObject()      {}
+func (o ObjectFunction) String() string { return "<function>" }
+func (o ObjectFunction) isObject()      {}
 
 type ObjectClosure struct {
 	// Env      *Env
@@ -58,5 +58,5 @@ type ObjectClosure struct {
 	// Bytecode *Bytecode
 }
 
-func (o *ObjectClosure) String() string { return "<closure>" }
-func (o *ObjectClosure) isObject()      {}
+func (o ObjectClosure) String() string { return "<closure>" }
+func (o ObjectClosure) isObject()      {}
