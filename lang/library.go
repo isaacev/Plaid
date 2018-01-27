8150ebd94c4baff36d2053ec04769cbe5b6f8541
isaacev <isaacevavold@gmail.com>
2018-01-27 13:23:47 -0600

    make library specific to ObjectBuiltin

--- lang/library.go
+++ lang/library.go
@@ -4,17 +4,17 @@ import "plaid/lang/types"
 
 type Library struct {
 	symbols map[string]*UniqueSymbol
-	objects map[*UniqueSymbol]Object
+	objects map[*UniqueSymbol]ObjectBuiltin
 }
 
-func (l *Library) addObject(name string, obj Object) {
+func (l *Library) addObject(name string, obj ObjectBuiltin) {
 	sym := &UniqueSymbol{name}
 	l.symbols[name] = sym
 	l.objects[sym] = obj
 }
 
 func (l *Library) Function(name string, typ types.Function, fn func(args []Object) (Object, error)) {
-	l.addObject(name, &ObjectBuiltin{
+	l.addObject(name, ObjectBuiltin{
 		typ: typ,
 		val: fn,
 	})
@@ -23,6 +23,6 @@ func (l *Library) Function(name string, typ types.Function, fn func(args []Objec
 func MakeLibrary(name string) *Library {
 	return &Library{
 		symbols: make(map[string]*UniqueSymbol),
-		objects: make(map[*UniqueSymbol]Object),
+		objects: make(map[*UniqueSymbol]ObjectBuiltin),
 	}
 }
